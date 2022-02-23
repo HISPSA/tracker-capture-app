7025032360a754671b849a2a1c92cc966d0e6e0e
Tony Valle <tony@devotta.no>
2022-02-23 10:13:21 +0100

    fix: in case search result is a single TEI in the selected orgunit

--- components/home/search/search-controller.js
+++ components/home/search/search-controller.js
@@ -204,7 +204,7 @@ trackerCapture.controller('SearchController',function(
                         //Open TEI if unique and in same search scope and in selected org unit
                         if(gridData.rows.own.length ===1 && res.callingScope === res.resultScope && searchGroup.uniqueGroup){
                             $scope.searching = null;
-                            openTei(gridData.rows.own[0]);
+                            openTei(gridData.rows.own[0], [$scope.selectedOrgUnit.id]);
                             return;
                         }
                     }
@@ -218,7 +218,7 @@ trackerCapture.controller('SearchController',function(
             }
         }
 
-        var openTei = function(tei, fromAudit, programOwners){
+        var openTei = function(tei, programOwners, fromAudit){
             if($scope.searchingForRelatedTei) {
                 $rootScope.$broadcast('assignRelationshipTei', tei);
             } else {
@@ -411,7 +411,7 @@ trackerCapture.controller('SearchController',function(
                 var def = $q.defer();
                 def.resolve();
                 if(res.action === "OPENTEI"){
-                    openTei(res.tei, res.fromAudit, res.programOwners);
+                    openTei(res.tei, res.programOwners, res.fromAudit);
                 }else if(res.action === "OPENREGISTRATION")
                 {
                     var registrationPrefill = getRegistrationPrefill(searchGroup);
